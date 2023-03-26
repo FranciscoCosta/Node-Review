630ac17a5db2fde4d2ee0d521901352912b247e6
Xico <francisco100eg@gmail.com>
2023-03-26 10:13:56 -0300

    :Npm: Npm

--- 03_Npm/index.js
+++ 03_Npm/index.js
@@ -6,3 +6,9 @@ console.log("Hello World!");
 
 //npm install date-fns
 //goes to dependencies in package.json in productions
+
+//npm install --save-dev date-fns
+//goes to devDependencies in package.json in development
+
+const { format } = require("date-fns");
+console.log(format(new Date(), "dd/MM/yyyy"));
