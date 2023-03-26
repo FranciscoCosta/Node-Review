f4dbfb98de946723a455a6be2a466b6e1598d8ba
Xico <francisco100eg@gmail.com>
2023-03-26 10:05:21 -0300

    :Npm: Adding npm to projects

---
+++ Npm/index.js
@@ -0,0 +1,8 @@
+//istall globaly
+//npm install -g nodemon
+console.log("Hello World!");
+
+//npm init -y to start package.json
+
+//npm install date-fns
+//goes to dependencies in package.json in productions
